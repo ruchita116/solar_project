f587862bf5eb70a83a1b1ab58ed7df7c0234510a
Dinesh Suthar <dineshsuthar2393@gmail.com>
2024-12-13 11:35:59 +0530

    .

--- client1/src/Component/Dashboard.jsx
+++ client1/src/Component/Dashboard.jsx
@@ -1,38 +1,37 @@
 import React, { useEffect } from 'react'
-import { useNavigate,Link } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { IoIosAdd } from "react-icons/io";
 import { FaUsersViewfinder } from "react-icons/fa6";
 
-const Dashboard = ({isAuth}) => {
+const Dashboard = ({ isAuth }) => {
   let navigate = useNavigate()
   useEffect(() => {
 
     if (isAuth != true)
-        navigate('/signup')
-}, [isAuth])
+      navigate('/signup')
+  }, [isAuth])
   return (
     <>
-    <div className='dashboard' >
-    <div className='button-container'>
-      
-      <div style={{ display: 'flex', backgroundColor: '#0295B6', padding: '10px', borderRadius: '10px' }}>
-        <div style={{ color: '#fff', margin: '10px' }}><i class="fa-solid fa-user fa-4x" style={{ margin: '5px' }}></i></div>
-        <div style={{ color: '#fff', margin: '10px', display: 'flex', alignItems: 'center', fontWeight: 'bold', fontFamily: 'serif', fontSize: '30px' }}>
-          <div style={{ margin: '5px' }}>
-           <Link to="wcr" style={{textDecoration:"none",color:"white"}}>+Add User</Link> 
-          </div>
+      <div className='dashboard' >
+        <div className='button-container'>
+          <Link to='wcr' style={{ display: 'flex', backgroundColor: '#0295B6', padding: '10px', borderRadius: '10px', textDecoration:'none' }}>
+            <div style={{ color: '#fff', margin: '10px' }}><i class="fa-solid fa-user fa-4x" style={{ margin: '5px' }}></i></div>
+            <div style={{ color: '#fff', margin: '10px', display: 'flex', alignItems: 'center', fontWeight: 'bold', fontFamily: 'serif', fontSize: '30px' }}>
+              <div style={{ margin: '5px' }}>
+                <div style={{ textDecoration: "none", color: "white" }}>+Add User</div>
+              </div>
+            </div>
+          </Link>
+          <Link to='details' style={{ display: 'flex', backgroundColor: '#0295B6', padding: '10px', borderRadius: '10px', textDecoration:'none' }}>
+            <div style={{ color: '#fff', margin: '10px' }}><i class="fa-solid fa-users fa-4x" style={{ margin: '5px' }}></i></div>
+            <div style={{ color: '#fff', margin: '10px', display: 'flex', alignItems: 'center', fontWeight: 'bold', fontFamily: 'serif', fontSize: '30px' }}>
+              <div style={{ margin: '5px' }}>
+                <div style={{ textDecoration: "none", color: "white" }}> View User</div>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
-      <div style={{ display: 'flex', backgroundColor: '#0295B6', padding: '10px', borderRadius: '10px' }}>
-        <div style={{ color: '#fff', margin: '10px' }}><i class="fa-solid fa-users fa-4x" style={{ margin: '5px' }}></i></div>
-        <div style={{ color: '#fff', margin: '10px', display: 'flex', alignItems: 'center', fontWeight: 'bold', fontFamily: 'serif', fontSize: '30px' }}>
-          <div style={{ margin: '5px' }}>
-          <Link to="details" style={{textDecoration:"none",color:"white"}}> View User</Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
     </>
   )
 }
